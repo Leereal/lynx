d314a90a1417a265288c353d811997d35d512918
Leereal <leereal08@ymail.com>
2023-10-08 16:24:47 +0200

    Initial Commit

---
+++ pages/index.vue
@@ -0,0 +1,22 @@
+<template>
+  <HeroSection />
+</template>
+<script setup lang="ts">
+useHead({
+  title: "Lynx : URL shortener",
+  meta: [
+    {
+      name: "author",
+      content: "Liberty Mutabvuri",
+    },
+    {
+      name: "description",
+      content: "Lynx, the best link shortener for free",
+    },
+    {
+      name: "keywords",
+      content: "Lynx, Link Shortener",
+    },
+  ],
+});
+</script>
